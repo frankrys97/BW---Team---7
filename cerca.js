2ee204462af7a07897215b72cb842774f422e107
Francesco Cristiano <frankrys97@gmail.com>
2024-04-17 12:43:58 +0200

    adds

--- cerca.js
+++ cerca.js
@@ -49,16 +49,19 @@ const onMouseMove = (event) => {
 const onMouseUp = () => {
   document.removeEventListener("mousemove", onMouseMove);
 };
+
+const leftBar = document.getElementById("leftBar");
+const rightBar = document.getElementById("rightBar");
 const closeRightBar = document.querySelector(".closeRightBar");
 
 closeRightBar.addEventListener("click", () => {
-  rightBar.style.display = "none";
+  rightBar.classList.remove("d-lg-block");
 });
 
 const notifications = document.getElementById("notifications");
 
 notifications.addEventListener("click", () => {
-  rightBar.style.display = "block";
+  rightBar.classList.add("d-lg-block");
 });
 
 const randomColor = () => {
@@ -79,3 +82,48 @@ const assignRandomColor = () => {
 };
 
 assignRandomColor();
+
+const dragHandle = document.getElementById("dragHandle");
+
+let isResizing = false;
+let lastX;
+const maxLeftBarWidth = 700;
+const rightTreshhold = 600;
+
+dragHandle.addEventListener("mousedown", (e) => {
+  isResizing = true;
+  lastX = e.clientX;
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!isResizing) return;
+
+  console.log(isResizing);
+
+  const delta = e.clientX - lastX;
+  const newWidth = leftBar.offsetWidth + delta;
+
+  const minWidth = 200;
+  if (newWidth >= minWidth && newWidth <= maxLeftBarWidth) {
+    leftBar.style.width = `${newWidth}px`;
+
+    const homeWidth = document.getElementById("home").offsetWidth;
+    const centerBar = document.getElementById("centerBar");
+    const rightBar = document.getElementById("rightBar");
+    const centerBarWidth = homeWidth - newWidth - rightBar.offsetWidth;
+    centerBar.style.width = `${centerBarWidth}px`;
+
+    if (newWidth >= rightTreshhold) {
+      rightBar.style.display = "none";
+    } else {
+      rightBar.style.display = "block";
+    }
+  }
+
+  lastX = e.clientX;
+});
+
+document.addEventListener("mouseup", () => {
+  isResizing = false;
+  console.log(isResizing);
+});
