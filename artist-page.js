1ce33a7f31f551a757b190dc925dd0b401a90f69
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-04-18 23:51:34 +0200

    zzzzz

--- artist-page.js
+++ artist-page.js
@@ -335,6 +335,8 @@ const findArtist = () => {
     });
 };
 findArtist();
+const randomizeSongs = [];
+
 const findPlaylistLeft = (url) => {
   fetch(url, {
     headers: {
@@ -390,6 +392,69 @@ const findPlaylistLeft = (url) => {
 const shuffleArray = (array) => {
   return array.sort(() => Math.random() - 0.5);
 };
+
+const dragHandle = document.getElementById("dragHandle");
+const leftBar = document.getElementById("leftBar");
+const rightBar = document.getElementById("rightBar");
+let isResizing = false;
+let lastX;
+const maxLeftBarWidth = 700;
+const rightTreshhold = 600;
+
+dragHandle.addEventListener("mousedown", (e) => {
+  isResizing = true;
+  lastX = e.clientX;
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!isResizing) return;
+
+  console.log(isResizing);
+
+  const delta = e.clientX - lastX;
+  const newWidth = leftBar.offsetWidth + delta;
+
+  const minWidth = 200;
+  if (newWidth >= minWidth && newWidth <= maxLeftBarWidth) {
+    leftBar.style.width = `${newWidth}px`;
+
+    const homeWidth =
+      document.getElementById("home").offsetWidth;
+    const centerBar = document.getElementById("centerBar");
+    const rightBar = document.getElementById("rightBar");
+    const centerBarWidth =
+      homeWidth - newWidth - rightBar.offsetWidth;
+    centerBar.style.width = `${centerBarWidth}px`;
+
+    if (newWidth >= rightTreshhold) {
+      rightBar.style.display = "none";
+    } else {
+      rightBar.style.display = "block";
+    }
+  }
+
+  lastX = e.clientX;
+});
+
+document.addEventListener("mouseup", () => {
+  isResizing = false;
+  console.log(isResizing);
+});
+
+const closeRightBar = document.querySelector(
+  ".closeRightBar"
+);
+
+closeRightBar.addEventListener("click", () => {
+  rightBar.classList.remove("d-lg-block");
+});
+
+const notifications =
+  document.getElementById("notifications");
+
+notifications.addEventListener("click", () => {
+  rightBar.classList.add("d-lg-block");
+});
 
 window.onload = () => {
   fetch(URL, {
